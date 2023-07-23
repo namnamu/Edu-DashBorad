11d95fb66a11b9b4f0f5d20fd471be73902fca5c
namnamu <skadlsrud8@naver.com>
2023-07-23 22:51:41 +0900

    merged line

--- pie.py
+++ pie.py
@@ -1,74 +1,114 @@
-from dash import Dash, dcc, html, Input, Output, callback, dash_table
+from dash import Dash, dcc, html, Input, Output, callback
 import plotly.express as px
 import plotly.graph_objects as go
 import pandas as pd
 
+def inputdata(filename):
+    df=pd.read_excel(filename)
+    df=df.groupby(['행정구역별','시점']).agg({'매우만족':'sum','약간만족':'sum','보통':'sum','약간불만족':'sum','매우불만족':'sum'}).reset_index()
+    return df
+
 
 app = Dash(__name__)
 
-# 데이터 선언 함수
-def inputdata(filename):
-    df=pd.read_excel(filename) #파일 읽어오기
-    #데이터 전처리
-    df=df.groupby(['행정구역별','시점']).agg({'매우만족':'sum','약간만족':'sum','보통':'sum','약간불만족':'sum','매우불만족':'sum'}).reset_index()
 
-    return df
+filename='17개 시도별 학생의 학교생활만족도 & 교원 1인당 학생수.xlsx'
+pie_df=inputdata(filename)
+df = pd.read_excel(filename)# line에서 사용할 df
 
-# 데이터프레임 선언(변수명 df)
-df=inputdata('17개 시도별 학생의 학교생활만족도 & 교원 1인당 학생수.xlsx')
-# 조회할 데이터.(컬럼 명으로 초기화한다.)
+
 slider_option='시점'
-dropdown_option='행정구역별'
+dropdown_option = '행정구역별'
+a = '시점'
+b = '재적학생수'
+c = '교원수'
 
-# 화면 구성.
-app.layout = html.Div([
-    # 슬라이더
-    html.Div(
-        dcc.Slider( 
-            df[slider_option].min(), # 최소 숫자부터
-            df[slider_option].max(), # 최대 숫자까지
-            step=None, # 건너뛸 정도
-            id='crossfilter-year--slider',
-            value=df[slider_option].max(), # 초기 값
-            marks={str(year): str(year) for year in df[slider_option].unique()} # 년도를 표기하기 위한 마커
-        ), style={'width': '49%', 'padding': '0px 20px 20px 20px'}),
-    # 드롭다운
-    html.Div(
+
+# 화면 구성 요소
+dropdown=html.Div(
         dcc.Dropdown(
-            id='crossfilter-loc--dropdown',
-            options=df[dropdown_option].unique(), # 해당 컬럼의 종류
-            value=df[dropdown_option].unique()[0], # 초기 값
-            clearable=False
-        ), style={'width': '49%', 'padding': '0px 20px 20px 20px'}),
+            id='crossfilter-dropdown-selection',
+            options=pie_df[dropdown_option].unique(),
+            value=pie_df[dropdown_option].unique()[0], clearable=False
+        ), style={'width': '49%', 'padding': '0px 20px 20px 20px'})
+
+pie = html.Div([
+    html.H2(children="만족도 비율",style={'textAlign':'center'}),
     # 그래프
     html.Div(
         dcc.Graph(
             id='graph-with-slider'
-        ),style={'width': '50%', 'padding': '0px 20px 20px 20px'}),
+        )
+    ),
+    # 슬라이더
+    html.Div(
+        dcc.Slider(  
+            pie_df[slider_option].min(),
+            pie_df[slider_option].max(),
+            step=None,
+            id='crossfilter-year--slider',
+            value=pie_df[slider_option].max(),
+            marks={str(year): str(year) for year in pie_df[slider_option].unique()} # 년도를 표기하기 위한 마커
+        )
+    )
+],style={'width': '39%','display':'inline-block','padding': '0px 20px 20px 20px'})
+
+line = html.Div([
+    html.H2(children='연도별 학생수와 교원수', style={'textAlign': 'center'}),
+    dcc.Graph(id='graph-content'),
+
+],style={'width': '39%', 'padding': '0px 20px 20px 20px','display':'inline-block'})
+
+
+
+# 화면 구성
+app.layout=html.Div([
+    dropdown,
+    line,
+    pie
 ])
 
-# 조건 이벤트
+# 이벤트
+@callback(
+    Output('graph-content', 'figure'),
+    Input('crossfilter-dropdown-selection', 'value'),
+)
+def update_graph(value):
+    dff = df[df[dropdown_option] == value]
+
+    fig = go.Figure()
+
+    fig.add_trace(go.Scatter(x=dff[a], y=dff[b], name='학생수', mode='lines+markers', yaxis='y1'))
+    fig.add_trace(go.Scatter(x=dff[a], y=dff[c], name='교원수', mode='lines+markers', yaxis='y2'))
+
+    fig.update_layout(
+        xaxis_title='연도',
+        yaxis_title='재적학생수',
+        yaxis2=dict(
+            title='교원수',
+            overlaying='y',
+            side='right'
+        )
+    )
+
+    return fig
+
 @callback(
     Output('graph-with-slider', 'figure'),
     Input(component_id='crossfilter-year--slider', component_property='value'),
-    Input(component_id='crossfilter-loc--dropdown', component_property='value'))
-def update_figure(slider_data,dropdown_data): #콜백에서 들어올 input데이터 순서대로
-    df=inputdata('17개 시도별 학생의 학교생활만족도 & 교원 1인당 학생수.xlsx') # 옵션들은 들고 들어오면서 df는 왜 못 가져오지?
-    # -> 병합시 통째로 함수형으로 만들거나, 클래스로 묶어버려야할 것 같다. 
+    Input(component_id='crossfilter-dropdown-selection', component_property='value'))
+def update_figure(slider_data,dropdown_data):
+    pie_df=inputdata(filename)
     
-    # 조건 검색 기능
-    df=df[df[slider_option].apply(lambda x: True if x==slider_data else False)]
-    df=df[df[dropdown_option].apply(lambda x: True if x==dropdown_data else False)]
+    pie_df=pie_df[pie_df[slider_option].apply(lambda x: True if x==slider_data else False)]
+    pie_df=pie_df[pie_df[dropdown_option].apply(lambda x: True if x==dropdown_data else False)]
     
-    labels = df.columns[2:] # 컬럼명
-    values = list(df.iloc[0,2:]) #각 해당하는 값(직접 명시된 %)
-    
-    # 그래프 초기화
+    labels = pie_df.columns[2:]
+    values = list(pie_df.iloc[0,2:])
+
     fig = go.Figure(data=[go.Pie(labels=labels, values=values, hole=.3)])
     fig.update_layout(transition_duration=500)
-    # output전달
     return fig
-
 
 if __name__ == '__main__':
     app.run(debug=True)
